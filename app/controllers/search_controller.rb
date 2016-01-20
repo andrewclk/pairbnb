d80063cd294a1fc0528ccdf8cdd8adf21c830f5e
Next <nextacademy@email.com>
2016-01-20 15:52:49 +0800

    Heroku-ing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,11 @@
 class SearchController < ApplicationController
-	Listing.reindex
+	# Listing.reindex
 	def search
 	  if params[:q].nil?
 	    @listings = []
 	  else
-	    @listings = Listing.search params[:q]
+	    @listings = Listing.search params[:q], fields: [:name, :address, :city]
+	    # byebug
 	  end
 	end
 end
--- db/migrate/20160112083943_create_listings.rb
+++ db/migrate/20160112083943_create_listings.rb
@@ -2,9 +2,9 @@ class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
       t.references :user, index: true 
-      t.text :name
-      t.text :address
-      t.text :city
+      t.text :name, index: true
+      t.text :address, index: true
+      t.text :city, index: true
       t.integer :price
       t.text :tag
       t.boolean :booking_status
